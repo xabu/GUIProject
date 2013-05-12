9a208cc8a3d0aa963948804e35a14ab3c742c68c
entropyftw <entropy.ftw@gmail.com>
2013-05-12 17:04:40 -0300

    Update Player.java

--- src/GUIProject/Player.java
+++ src/GUIProject/Player.java
@@ -11,9 +11,13 @@ public class Player {
 	private int points;
 	private File highScores;
 	private Point loc;
-	/**
-	 * 
-	 */
+
+	highScores = new File ("src/GUIProject/HighScores.txt");
+	
+	if (!highScores.exists()){
+		highScores.createNewFile();
+	}
+	
 	public Player(){
 		
 	}
@@ -21,7 +25,7 @@ public class Player {
 	 * 
 	 */
 	public void addScore(){
-		
+		highScores
 	}
 	public Point getLoc(){
 		return loc;
